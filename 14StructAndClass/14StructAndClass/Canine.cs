4f254af00411b5ee5da64499212421b588992545
lu <1686143231@qq.com>
2016-08-27 13:58:30 +0800

    C#中的结构体

---
+++ 14StructAndClass/14StructAndClass/Canine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StructAndClass
+{
+    class Canine
+    {
+        public string Name;
+        public string Breed;
+
+        public Canine(string name, string breed)
+        {
+            this.Name = name;
+            this.Breed = breed;
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("My name is {0} and I'm a {1}.", Name, Breed);
+        }
+    }
+}
