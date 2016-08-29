9a07c44297d64a71a2167c806531f1dfbe6a78cc
lu <1686143231@qq.com>
2016-08-29 21:53:50 +0800

    Type Exercise

---
+++ 14TypeExamples/14TypeExamples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Type练习
+namespace TypeExamples
+{
+
+    class Program
+    {
+        class NestedClass
+        {
+            public class DoubleNestedClass
+            { 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Type nestedClassType = typeof(NestedClass.DoubleNestedClass);
+            Console.WriteLine(nestedClassType.FullName);
+
+            List<string> stringList = new List<string>();
+            Console.WriteLine(stringList.GetType().Name);
+        }
+    }
+}
